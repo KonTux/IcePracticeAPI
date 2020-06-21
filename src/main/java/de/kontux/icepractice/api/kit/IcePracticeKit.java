63d8ccf6664895bc7353f65f64be3f7ad6199974
KonTux <kontuxmail@gmail.com>
2020-06-21 08:58:50 +0200

    Added regen/uhc flag for kits

--- src/main/java/de/kontux/icepractice/api/kit/IcePracticeKit.java
+++ src/main/java/de/kontux/icepractice/api/kit/IcePracticeKit.java
@@ -56,4 +56,8 @@ public interface IcePracticeKit {
     boolean isEditable();
 
     void setEditable(boolean editable);
+
+    boolean allowRegen();
+
+    void setAllowRegen(boolean allowRegen);
 }
